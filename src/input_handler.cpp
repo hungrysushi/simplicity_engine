60c44337656cd4ff08bf7bbb98a269fcd3dd860b
Ashwin Chandrasekar <achandr5@ncsu.edu>
2017-12-06 23:32:34 -0500

    Add WASD checks to input handler

--- src/input_handler.cpp
+++ src/input_handler.cpp
@@ -28,9 +28,27 @@ InputHandlerError InputHandler::ProcessInput() {
         // outside of the engine update loop
         glfwMakeContextCurrent(window_);        
 
+        // handle various input keys
+
         if (glfwGetKey(window_, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
                 std::cout << "Escape key pressed. Exiting now." << std::endl;
                 glfwSetWindowShouldClose(window_, true);
+        }
+
+        if (glfwGetKey(window_, GLFW_KEY_W) == GLFW_PRESS) {
+                std::cout << "W pressed." << std::endl;
+        }
+
+        if (glfwGetKey(window_, GLFW_KEY_A) == GLFW_PRESS) {
+                std::cout << "A pressed." << std::endl;
+        }
+
+        if (glfwGetKey(window_, GLFW_KEY_S) == GLFW_PRESS) {
+                std::cout << "S pressed." << std::endl;
+        }
+
+        if (glfwGetKey(window_, GLFW_KEY_D) == GLFW_PRESS) {
+                std::cout << "D pressed." << std::endl;
         }
 
         // maybe this should happen before processing input...
