201c294b6f01c03576abd0cab586029f24d8961d
Ashwin Chandrasekar <ashw.chandr@gmail.com>
2019-05-11 18:28:35 -0400

    Physics engine initial stubs

---
+++ newton/src/newton.cpp
@@ -0,0 +1,13 @@
+#include "newton.h"
+
+namespace newton {
+
+Classical::Classical() {
+        // nothing to do yet
+}
+
+Classica::~Classical() {
+        // nothing to do yet
+}
+
+} /* namespace newton */
